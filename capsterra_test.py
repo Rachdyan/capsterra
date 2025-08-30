e7bed7f34ee7deb6e30d329b2009f28da5cb6065
InnovativeOptics <rachdyannaufal@gmail.com>
2025-08-30 09:59:54 +0700

    update cdp

--- capsterra_test.py
+++ capsterra_test.py
@@ -92,8 +92,8 @@ def scrape_category(row):
             maximize=True) as sb:
         link = row['cloud_link']
         # sb.cdp.open(link)
-        sb.uc_open(link)
-        # sb.uc_gui_click_captcha()
+        sb.activate_cdp_mode(link)
+        sb.uc_gui_click_captcha()
         sb.uc_gui_click_cf()
         # sb.handle_ removed; not a valid method
         print(f"Scraping Category: {row['category_name']}")
